f4a66a65c4b531cd38a8196f291eda033bbf779e
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-26 16:37:09 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (aryDrivers) => {
+  return aryDrivers.length >= 2 ? aryDrivers.slice(0,2) : [];
+}
+
+const returnLastTwoDrivers = (aryDrivers) => {
+  if (aryDrivers.length >= 2) {
+    return aryDrivers.slice(-2)
+  }
+  return []; 
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (num) => {
+  return (fare) => num * fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(aryDrivers, cb) {
+  return cb(aryDrivers)
+}
